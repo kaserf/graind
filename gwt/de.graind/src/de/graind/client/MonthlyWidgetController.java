c7afd0c23c744a19cad0ab75a7a58f7edd828d7f
Felix Kaser <f.kaser@gmx.net>
2012-02-04 21:40:16 +0100

    remove annoying alert

--- gwt/de.graind/src/de/graind/client/MonthlyWidgetController.java
+++ gwt/de.graind/src/de/graind/client/MonthlyWidgetController.java
@@ -1,5 +1,6 @@
 package de.graind.client;
 
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.gdata.client.DateTime;
 import com.google.gwt.gdata.client.EventEntry;
 import com.google.gwt.gdata.client.GData;
@@ -10,7 +11,6 @@ import com.google.gwt.gdata.client.calendar.CalendarEventQuery;
 import com.google.gwt.gdata.client.calendar.CalendarService;
 import com.google.gwt.gdata.client.impl.CallErrorException;
 import com.google.gwt.i18n.client.DateTimeFormat;
-import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
 import de.graind.shared.Config;
@@ -25,7 +25,7 @@ public class MonthlyWidgetController implements MonthlyWidgetView.Controller {
     this.view = view;
 
     if (GData.isLoaded(GDataSystemPackage.CALENDAR)) {
-      Window.alert("Calendar package is loaded");
+      GWT.log("Calendar package is loaded");
       gdataLoaded();
     } else {
       GData.loadGDataApi(Config.API_KEY, new Runnable() {
@@ -38,7 +38,6 @@ public class MonthlyWidgetController implements MonthlyWidgetView.Controller {
 
   private void gdataLoaded() {
     this.service = CalendarService.newInstance(Config.APPLICATION_NAME);
-    // initCalendar();
     MonthlyWidgetController.this.view.init(MonthlyWidgetController.this);
   }
 
