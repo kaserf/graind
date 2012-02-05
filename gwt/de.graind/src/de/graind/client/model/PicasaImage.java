fcf9e50164b28bfa02be26835eb1a4446fdded5a
Felix Kaser <f.kaser@gmx.net>
2012-02-05 17:39:18 +0100

    added model for picasa images

---
+++ gwt/de.graind/src/de/graind/client/model/PicasaImage.java
@@ -0,0 +1,73 @@
+package de.graind.client.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class PicasaImage implements Serializable {
+  private static final long serialVersionUID = 1L;
+
+  private String id;
+  private String url;
+  private String title;
+  private String albumId;
+  private Date timestamp;
+  private Long width;
+  private Long height;
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getUrl() {
+    return url;
+  }
+
+  public void setUrl(String url) {
+    this.url = url;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getAlbumId() {
+    return albumId;
+  }
+
+  public void setAlbumId(String albumId) {
+    this.albumId = albumId;
+  }
+
+  public Date getTimestamp() {
+    return timestamp;
+  }
+
+  public void setTimestamp(Date timestamp) {
+    this.timestamp = timestamp;
+  }
+
+  public Long getWidth() {
+    return width;
+  }
+
+  public void setWidth(Long width) {
+    this.width = width;
+  }
+
+  public Long getHeight() {
+    return height;
+  }
+
+  public void setHeight(Long height) {
+    this.height = height;
+  }
+
+}
